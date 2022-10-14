2b348cba1b3a413cbadf5e0775b51b36727d968f
Aboubakr Deramchi <aboubakarderamchi@gmail.com>
2022-10-14 17:38:35 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -9,13 +9,13 @@ $(document).ready (function (){
 
 $(document).on('click', '.arabic', function(){
 
-    window.open('/arabic.html')
+    window.open('./arabic.html')
    
 
 })
 $(document).on('click', '.français', function(){
 
-    window.open('/homepage.html')
+    window.open('./homepage.html')
 
 })
 
@@ -23,8 +23,8 @@ $(document).on('click', '.français', function(){
 // ***********************Display calculation box and overly********************
 // ************************************************ -->
 
-$(document).on('click', '.left-arrow,#overly', function(){
-
+$(document).on('click', '#overly','#return', function(){
+  
     $('.calculation-box').fadeOut()
     $('#overly').fadeOut()
     
@@ -37,6 +37,10 @@ $(document).on('click', '.left-arrow,#overly', function(){
 $(document).on('click', '.button', function (){
     $('.calculation-box').fadeIn()
     $('#overly').fadeIn()
+    // $('#total').fadeIn()
+    // $('#tva').fadeIn()
+    // $('#prixhtdzd').fadeIn()
+    // $('#return').fadeIn()
 
 
 
@@ -62,12 +66,11 @@ function maFonction (prixV, changeP) {
     var tva = parseInt(((taxe+(prixHtdzd))*0.19));
     var total = parseInt((prixHt*changeP)+taxe+tva+(500*changeP));
     
-    $('.calculation-box').find("#prixhtdzd").html(prixHtdzd)
-    $('.calculation-box').html("Total = "+ total + "  dzd (Frais de bateau inclus : 107 500 dzd)")
+   
+    $('#total').html("Total = "+ total + "dzd (Frais de transport en bateau inclus : 107 500 dzd)") 
+    $('#prixhtdzd').html("Total = "+ prixV) 
     
     }
 
-// <!-- ************************************************
-// ***********************Display Total Function********************
-// ************************************************ -->
+
 
