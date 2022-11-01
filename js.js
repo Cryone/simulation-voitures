4427b5ef7cf7890167d45645d906bc7430748932
Aboubakr Deramchi <aboubakarderamchi@gmail.com>
2022-11-01 11:57:53 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -12,6 +12,7 @@ $(document).on('click', '.ar', function(){
     displayAr()
     $(".ar").css({"background-color":"rgb(239, 79, 79)","border-radius":"10px"})
     $(".fr").css("background-color","transparent")
+    $(".en").css("background-color","transparent")
     
 })
 
@@ -19,6 +20,15 @@ $(document).on('click', '.ar', function(){
 $(document).on('click', '.fr', function(){
 
     displayFr()
+    $(".en").css("background-color","transparent")
+})
+
+$(document).on('click', '.en', function(){
+
+    displayEn()
+    $(".en").css({"background-color":"rgb(239, 79, 79)","border-radius":"10px"})
+    $(".fr").css("background-color","transparent")
+    $(".ar").css("background-color","transparent")
 
 })
 
@@ -80,7 +90,9 @@ function maFonction (prixV, changeP) {
     var taxe = parseInt(prixHtdzd*0.32);
     var tva = parseInt(((taxe+(prixHtdzd))*0.19));
     var total = parseInt((prixHt*changeP)+taxe+tva+(500*changeP));
-    
+
+    var commas = total.toLocaleString("en-US");
+    var commas = total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
    
     html =`
     
@@ -88,20 +100,11 @@ function maFonction (prixV, changeP) {
             <div id="prixhtdzd" class="grid  inline-block p10">Prix hors taxe de la voiture en dzd = <span class='bold'> ${prixHtdzd} dzd </span>. (- TVA de 20%)</div> 
             <div id="taxe" class="grid  inline-block p10">Taxe douanière appliquée (32%) = <span class='bold'> ${taxe} dzd </span> </div>
             <div id="tva" class="grid  inline-block p10">TVA appliquée = <span class='bold'> ${tva} dzd </span></div>
-            <div id="total" class="grid center text-center text-white p10"><span>Total (Frais de transport en bateau inclus) (500 euros) = </span><span class='bold very-big-text'> ${total} dzd </span></div>
+            <div id="total" class="grid center text-center text-white p10"><span>Total (Frais de transport en bateau inclus) (~500 euros) = </span><span class='bold very-big-text'> ${commas},00 dzd (TTC) </span></div>
     `    
     $(".calculation-box-body").html(html)
     }
-    function commafy(num) {
-        var str = num.toString().split('.');
-        if (str[0].length >= 5) {
-            str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
-        }
-        if (str[1] && str[1].length >= 5) {
-            str[1] = str[1].replace(/(\d{3})/g, '$1 ');
-        }
-        return str.join('.');
-    }
+    
     function displayAr () {
 
           var html
@@ -117,11 +120,14 @@ function maFonction (prixV, changeP) {
               </div>
               <div class="grid langue center p10 ">
                   <div class="grid center fr p10">
-                      <button class="p10 big-text pointer text-black"><h4>Fr</h4></button>
+                      <button class="p10 small-text pointer text-black"><h4>Fr</h4></button>
                   </div>
                   <div class="grid center pointer ar p10">
-                      <button class="p10 big-text pointer text-white"><h4>ع</h4></button>
+                      <button class="p10 small-text pointer text-white"><h4>ع</h4></button>
                   </div>
+                  <div class="grid center pointer en p10">
+                  <button class="p10 small-text pointer "><h4>En</h4></button>
+                    </div>
                     
                       
       
@@ -231,11 +237,15 @@ html=`
     </div>
     <div class="grid langue center p10 ">
         <div class="grid center fr p10">
-            <button class="p10 big-text pointer text-white"><h4>Fr</h4></button>
+            <button class="p10 small-text pointer text-white"><h4>Fr</h4></button>
         </div>
         <div class="grid center pointer ar p10">
-            <button class="p10 big-text pointer text-black"><h4>ع</h4></button>
+            <button class="p10 small-text pointer text-black"><h4>ع</h4></button>
         </div>
+        <div class="grid center pointer en p10">
+        <button class="p10 small-text pointer"><h4>En</h4></button>
+        
+    </div>
           
             
 
@@ -345,6 +355,138 @@ html=`
 
 }
     
+
+function displayEn (){
+    var html
+    
+    html=`
+    <div class="grid container">
+    <nav class="grid navbar" >
+        <div class="grid header p10 ">
+            <div class="grid p10">
+                <div class="grid   big-title  text-red  ">Calculate the price of your future car</div>
+                <div class="grid  small-title   text-gray">Simulate the price of the car you want to buy</div>
+            </div>
+        </div>
+        <div class="grid langue center p10 ">
+            <div class="grid center fr p10">
+                <button class="p10 small-text pointer text-black"><h4>Fr</h4></button>
+            </div>
+            <div class="grid center pointer ar p10">
+                <button class="p10 small-text pointer text-black"><h4>ع</h4></button>
+            </div>
+            <div class="grid center pointer en p10">
+            <button class="p10 small-text pointer"><h4>En</h4></button>
+            
+        </div>
+              
+                
+    
+        </div>
+    </nav>
+    
+        <div class="grid main-container center p10 gap30">
+        
+            <img id="container-img" src="./img/img.png" alt="image">
+            <div class="grid calculation-box grid p10 shadow text-gray">
+                <div class="grid calculation-box-body ">
+                    <div id="" class='inline-block p10'> Détails de calcul : <span class="material-symbols-outlined text-white">restart_alt</span></div>
+                    <div id="prixhtdzd" class="grid w100 p10"></div> 
+                    <div id="taxe" class="grid w100 p10"></div>
+                    <div id="tva" class="grid w100 p10"></div>
+                    <div id="total" class="grid center text-red p10 text-center">
+                        <div id="calculation-box-footer" class=" grid pointer p10 bold ">
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="grid box p10 ">
+                            <!-- ************************************************
+                ***********************Français********************
+                ************************************************ -->
+                <div class="text-blue bold box-container">
+    
+                    <div id="prix-voiture" class="grid p10 text-gray big-text">Car price
+                    </div>
+                    <div class="grid p10 w100" style="margin-bottom: 10px">
+                        <form class="grid input-grid gap5">
+                            <input id="prixV" type="number" class="input-item" placeholder="example : 18000 euros">
+                            <button class="grid button-reset center text-white pointer"><span class="material-symbols-outlined">
+                                restart_alt
+                                </span>
+                            </button>
+                            
+                        </form>    
+                    </div>  
+                    <div id="prix-changeP" class="grid p10 text-gray">Parallel market exchange rate
+    
+                    </div>
+                    <div class="grid p10" style="margin-bottom: 10px">
+                        <form class="grid input-grid gap5">
+                            <input id="changeP" type="number" class="input-item" placeholder="example : 1 euro = 215 dzd"  class="input-item">  
+                            <button class="grid button-reset center text-white pointer"><span class="material-symbols-outlined">
+                                restart_alt
+                                </span>
+                            </button>
+                        </form>
+                    </div>
+    
+                </div>  
+    
+                <!-- ************************************************
+                ***********************CALCULATION-BOX********************
+                ************************************************ -->
+    
+    
+                <div class="grid">
+                    <div class="grid p10 ">
+    
+                        <button class=" total pointer p10 text-white ">Calculate the price</button> 
+                    </div>
+                </div>
+            </div>     
+        </div>
+                <!-- ************************************************
+                ***********************FOOTER********************
+                ************************************************ -->
+    <div class="grid center footer small-text text-gray">
+        @Copyright  Z / dev
+        
+    
+    </div>
+    
+           
+    </div>
+    
+    <div id="overly">
+    
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        `
+    
+        $("body").html(html)
+    
+    }
 
 
 
